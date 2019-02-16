59db230403d237e0412f347e1084829f0ed97963
Charles Gunn <cjgunnmail@gmail.com>
2019-02-15 20:46:36 -0500

    canny line

--- process_image.py
+++ process_image.py
@@ -14,6 +14,32 @@ def clean_image(img):
     return morphed
 
 
+def detect_lines(img):
+    low_threshold = 50
+    high_threshold = 150
+    img = cv2.Canny(img, low_threshold, high_threshold)
+
+    rho = 1  # distance resolution in pixels of the Hough grid
+
+    theta = np.pi / 180  # angular resolution in radians of the Hough grid
+    # minimum number of votes (intersections in Hough grid cell)
+    threshold = 15
+    min_line_length = 50  # minimum number of pixels making up a line
+    max_line_gap = 20  # maximum gap in pixels between connectable line segments
+    line_img = np.copy(img) * 0  # creating a blank to draw lines on
+
+    # Run Hough on edge detected image
+    # Output "lines" is an array containing endpoints of detected line segments
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]),
+                            min_line_length, max_line_gap)
+
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(line_img, (x1, y1), (x2, y2), (255, 0, 0), 5)
+
+    return img
+
+
 def show_imgs(*imgs):
     for e, img in enumerate(imgs):
         cv2.imshow(str(e), img)
@@ -22,6 +48,8 @@ def show_imgs(*imgs):
 
 
 if __name__ == "__main__":
-    img = cv2.imread("imgs/{}.JPG".format(4), 0)
-    post_img = clean_image(img)
-    show_imgs(post_img)
+    for i in range(1, 6):
+        img = cv2.imread("imgs/{}.JPG".format(i), 0)
+        post_img = clean_image(img)
+        line_img = detect_lines(post_img)
+        show_imgs(post_img, line_img)
