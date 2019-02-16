7ffcb335436675ff5d5d90adbc2115382444b671
Arya Kumar <thearyaskumar@icloud.com>
2019-02-15 21:57:34 -0500

    edge detection

--- process_image.py
+++ process_image.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from itertools import combinations
 
 
 def clean_image(img):
@@ -10,25 +11,35 @@ def clean_image(img):
     img = cv2.dilate(img, np.ones((5,5)), iterations=2)
     return img
 
-def corner_detection_image(img):
-    img = cv2.erode(img, np.ones((9, 9)), iterations=1)
-    return img
-
 
 def detect_lines(img):
     block_size = 6
     aperture = 15
     free_parameter = 0.04
 
-    cd_img = corner_detection_image(img)
+    img = cv2.erode(img, np.ones((9, 9)), iterations=1)
 
-    resps = cv2.cornerHarris(cd_img, block_size, aperture, free_parameter)
+    resps = cv2.cornerHarris(img, block_size, aperture, free_parameter)
     resps = cv2.dilate(resps, None, iterations=4)
     threshold = 0.1
 
-    corners = np.copy(cd_img)
-    # corners = cv2.cvtColor(corners, cv2.COLOR_GRAY2BGR)
-    corners[resps > threshold * resps.max()] = 0.
+    img = np.copy(img)
+    img[resps > threshold * resps.max()] = 0.
+
+    contours,hierarchy = cv2.findContours(img, 1, 2)
+
+    corners = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    for cnt in contours:
+        rect = cv2.minAreaRect(cnt)
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+
+        p1 = box[0]
+        p2 = min(box[1:], key=lambda x: np.linalg.norm(p1-x))
+
+        print(p1, p2)
+
+        im = cv2.drawContours(corners,[box],0,(0,0,255),2)
 
     return corners
 
