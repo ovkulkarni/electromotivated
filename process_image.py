9d1b561be40527f9f46ddafb5a81382ac7e530c3
Arya Kumar <thearyaskumar@icloud.com>
2019-02-15 21:39:57 -0500

    remove corners

--- process_image.py
+++ process_image.py
@@ -7,20 +7,28 @@ def clean_image(img):
     img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
                                         cv2.THRESH_BINARY_INV, 25, 50)
     img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, np.ones((7, 7)))
+    img = cv2.dilate(img, np.ones((5,5)), iterations=2)
+    return img
+
+def corner_detection_image(img):
+    img = cv2.erode(img, np.ones((9, 9)), iterations=1)
     return img
 
 
 def detect_lines(img):
-    block_size = 2
-    aperture = 29
+    block_size = 6
+    aperture = 15
     free_parameter = 0.04
-    resps = cv2.cornerHarris(img, block_size, aperture, free_parameter)
-    resps = cv2.dilate(resps, None, iterations=3)
+
+    cd_img = corner_detection_image(img)
+
+    resps = cv2.cornerHarris(cd_img, block_size, aperture, free_parameter)
+    resps = cv2.dilate(resps, None, iterations=4)
     threshold = 0.1
 
-    corners = np.copy(img)
-    corners = cv2.cvtColor(corners, cv2.COLOR_GRAY2BGR)
-    corners[resps > threshold * resps.max()] = [255, 0, 0]
+    corners = np.copy(cd_img)
+    # corners = cv2.cvtColor(corners, cv2.COLOR_GRAY2BGR)
+    corners[resps > threshold * resps.max()] = 0.
 
     return corners
 
